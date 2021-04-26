2455ad7a20c05a89a5bb503780f7c2f3a8c55508
DarrenDong0426 <79467048+DarrenDong0426@users.noreply.github.com>
2021-04-25 21:36:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function schoolSimulation(){
+    window.location.href = "School.html";
+}
+
+function socialSimulation(){
+  window.location.href = "Social.html";
+}
+
+function aggressionSimulation(){
+  window.location.href = "Aggression.html";
+}
+
+function generalSimulation(){
+  window.location.href = "General.html"; 
+}
+
+function back(){
+    window.location.href = "index.html"; 
+}
+
+function resources(){
+  window.location.href = "resources.html";
+}
